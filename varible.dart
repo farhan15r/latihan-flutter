a0425e93ab4043710f8fe38b558da70632d86e76
nak.rokesselalu <nak.rokesselalu@gmail.com>
2024-02-23 00:40:53 +0000

    belajar dasar dart

---
+++ varible.dart
@@ -0,0 +1,18 @@
+void main() {
+  var name = "Farhan Ramadhan"; // auto asign the data type
+  print(name);
+  // name = 123; // error
+  // print(name);
+
+  print("--------------------------");
+  Object objek = "ini pake object";
+  print(objek);
+  // wen can asign number to object (dinamic type)
+  objek = 123;
+  print(objek);
+  // also bool
+  objek = true;
+  print(objek);
+
+  print("--------------------------");
+}
